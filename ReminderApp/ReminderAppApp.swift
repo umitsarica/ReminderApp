8542b25321c6f8063fad23d89e0d18aa8e569eed
umitsarica <usarica@gmail.com>
2024-02-25 10:48:32 +0300

    Initial Commit

---
+++ ReminderApp/ReminderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderAppApp.swift
+//  ReminderApp
+//
+//  Created by Umit SARICA on 25.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ReminderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
